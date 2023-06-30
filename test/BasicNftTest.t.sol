b2bb683a7aa8b0c05e452d6d397ac24adbb2c516
Ritik <ritikagarwal570@gmail.com>
2023-06-30 16:03:08 +0530

    add

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -4,13 +4,29 @@ pragma solidity 0.8.18;
 
 import {Test} from "forge-std/Test.sol";
 import {BasicNft} from "../src/BasicNft.sol";
-import {DeployBasicNft} from "../script/DeployBasicNft.s.sol"; 
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 
-contract BasicNftTest is Test{
+contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("user");
 
     function setUp() external {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
 
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndhaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft("");
     }
 }
